7f0bd67a420b90264b4c44a6e9fa469b868af9d3
EINDEX <snowstarlbk@gmail.com>
2017-07-19 14:44:23 +0800

    Rss

--- blog/feeds.py
+++ blog/feeds.py
@@ -9,3 +9,21 @@ Copyright (C) 2016-2017 EINDEX Li
 @Created       : 2017/7/19
 @Last Modified : 2017/7/19
 """
+from django.contrib.syndication.views import Feed
+
+from .models import Post
+
+
+class AllPostsRssFeed(Feed):
+    title = 'EINDEX\'s Blog'
+    link = '/'
+    description = "EINDEX 的博客"
+
+    def items(self):
+        return Post.objects.all()
+
+    def item_title(self, item):
+        return '%s' % item.title
+
+    def item_description(self, item):
+        return item.body
--- blog/urls.py
+++ blog/urls.py
@@ -11,11 +11,13 @@ Copyright (C) 2016-2017 EINDEX Li
 """
 from django.conf.urls import url
 
+from blog.feeds import AllPostsRssFeed
 from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='detail'),
     url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.archives, name='archives'),
-    url(r'^tag/(?P<pk>[0-9]+)/$', views.TagView.as_view(), name='tags')
+    url(r'^tag/(?P<pk>[0-9]+)/$', views.TagView.as_view(), name='tags'),
+    url(r'^feed/$', AllPostsRssFeed(), name='rss')
 ]
